5b8ae46446daf507c87628c192fd01400e82c832
Han003 <jagolegion@gmail.com>
2019-10-03 13:14:45 +0200

    initial commit

---
+++ src/app/app.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'clock-root',
+  templateUrl: './app.component.html',
+  styleUrls: ['./app.component.scss']
+})
+export class AppComponent {
+  title = 'easyatwork-clock';
+}
